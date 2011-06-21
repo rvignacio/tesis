764814144847c4aa058fe264483390a61700dc4a
Ignacio Ríos Vazquez <rvignacio@gmail.com>
2011-06-21 06:44:13 -0700

    Formato del archivo. Cambio del texto de la variable title.

--- milestone1/app.js
+++ milestone1/app.js
@@ -1,48 +1,47 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express'), wordchecker = require('./libs/wordchecker');
+var express = require('express'),
+	wordchecker = require('./libs/wordchecker');
 
 var app = module.exports = express.createServer();
 
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'jade');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+	app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+	app.use(express.errorHandler()); 
 });
+
 
 // Routes
-
 app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Función sintáctica de una palabra'
-  });
+	res.render('index', {
+		title: 'Función sintáctica de una palabra'
+	});
 });
 app.get('/wordcheck',function(req,res){
-	  wordchecker.check(req.query.word,function(syntaxFunction){
+	wordchecker.check(req.query.word,function(syntaxFunction){
     	res.send('es '+(syntaxFunction.join(', ').replace(/,([^,]*)/,' y$1') || '... NO TENEMOS IDEA!!! =)'));
-    });
+    	});
 });
 
+
 // Only listen on $ node app.js
-
-
 if (!module.parent) {
-  app.listen(3000);
-  console.log("Express server listening on port %d", app.address().port);
+	app.listen(3000);
+	console.log("Express server listening on port %d", app.address().port);
 }
