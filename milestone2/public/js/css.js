3d57892950248430f12851611d0c586127f6de3c
Ignacio Ríos Vazquez <rvignacio@gmail.com>
2011-08-25 14:39:25 -0300

    Código comentado

--- milestone2/public/js/css.js
+++ milestone2/public/js/css.js
@@ -1,4 +1,6 @@
+//JS para layout de la página
 (function(){
+	//Quita los bordes que sobran de las listas de palabras
 	$('.lista:first').css('border-top', '0px');
 	$('.lista:last').css('border-bottom', '0px');
 })();
